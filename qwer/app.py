3be43aca88ca6d531f8b652eecff7b02e0c7f032
ernur2010-web <ernur2010@gmail.com>
2026-05-16 22:32:21 +0500

    Update app.py

--- qwer/app.py
+++ qwer/app.py
@@ -135,6 +135,5 @@ def product_detail(product_id):
 
 
 if __name__ == '__main__':
-
-    
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
+    port=int(os.environ.get('PORT',5000)))
+    app.run(host='0.0.0.0',port=port)
